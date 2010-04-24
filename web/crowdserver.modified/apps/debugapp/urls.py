fdfd7f46fbc481624003448e40449883a721b902
James Dennis <jdennis@gmail.com>
2010-04-24 18:16:56 -0400

    removed unnecessary back up

--- web/crowdserver.modified/apps/debugapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-from django.conf import settings
-from django.contrib import admin
-
-urlpatterns = patterns('',
-
-    (r'^makedata/(?P<how_many>\d+)/$', 'debugapp.views.makedata'),
-)
