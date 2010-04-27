22a3e9e2243d64b62d6cf364ee8d8f03e2c55fa5
James Dennis <jdennis@gmail.com>
2010-04-26 22:13:13 -0400

    Added default for date time and modified

--- web/crowdserver/apps/guestlist/models.py
+++ web/crowdserver/apps/guestlist/models.py
@@ -15,8 +15,8 @@ class Guest(models.Model):
     arrived = models.BooleanField(_('Arrived'), default=False)
     plus_count = models.IntegerField(_('Additional guests'), default=0)
     plus_counted = models.IntegerField(_('Guests counted'), default=0)
-    date_created = models.DateTimeField(_('Date Created'))
-    date_modified = models.DateTimeField(_('Date Modified'))
+    date_created = models.DateTimeField(_('Date Created'), default=datetime.now)
+    date_modified = models.DateTimeField(_('Date Modified'), default=datetime.now)
 
     def __unicode__(self):
         return u'%s' % (self.name)
